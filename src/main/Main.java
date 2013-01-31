93afcc050ff52e1dbfda6f881d5456cce277bbcf
tksBioinfo <mazda.takasky@gmail.com>
2013-01-31 14:03:06 +0900

    rls1no の実装を開始している。次は行列計算から。

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,6 +15,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("System starting");
+		
+		
 
 	}
 
--- src/rls/RegularizedLeastSquareRA.java
+++ src/rls/RegularizedLeastSquareRA.java
@@ -11,14 +11,32 @@ public class RegularizedLeastSquareRA{
 
 	/**
 	 * 
+	 * implements Regularized Least Square regression.
+	 * regparameter is recommended to be "ln(r) = 2.6"
+	 * independent(input)
+	 * dependent(target)
+	 * 
 	 * @param independent
 	 * @param dependent
+	 * @param regparameter
 	 * @return
 	 */
-	public static double[] coefficientset(double[] independent, double[] dependent) {
+	public static double[] coefficientset(double[][] independent, double[] dependent, double regparameter) {
+		double[] w = new double[independent.length];
+		int M = independent[0].length;
+		int N = independent.length;
 		
+		/*
+		 * 行列計算をする。
+		 */
+		double[][] dtd = new double[M][M];
+		for(int i = 0; i < M; i++){
+			for(int j = 0; j < N; j++){
+				
+			}
+		}
 		
-		return null;
+		return w;
 	}
 
 	/**
